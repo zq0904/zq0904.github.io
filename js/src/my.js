abc56966766f05e7fd39a18259ed7ef7efb70b61
zhaoqi <154809748@qq.com>
2018-03-18 16:28:56 +0800

    update

--- js/src/my.js
+++ js/src/my.js
@@ -1,8 +1,33 @@
 $(function () {
-  console.log(a);
-  //
-  if (window.location.pathname !== '/') {
-    $('.post-type-normal').css({background: 'none'})
-  }
+  showArticleMap();
+  rollColour();
 });
-var a = '测试';
+// 显示首页的文章底图 有底图没有红色背景
+function showArticleMap() {
+  if (window.location.pathname === '/') {
+    $('.post-type-normal').each(function (i, e) {
+      var $e = $(e);
+      if ($e.data('image')) {
+        e.style = $e.data('image');
+        $e.children('.post-block').css({
+          'background-color': 'rgba(0,0,0,0)'
+        });
+      }
+      $e = null;
+    });
+  }
+}
+// 博客副标题 动态颜色
+function rollColour() {
+  var $subtitle = $('.site-subtitle');
+  var i = 0;
+  window.setInterval(function () {
+    i++;
+    $subtitle.css({
+      'background-position': i + '%'
+    });
+    if (i === 95) {
+      i = -1;
+    }
+  }, 10);
+}
